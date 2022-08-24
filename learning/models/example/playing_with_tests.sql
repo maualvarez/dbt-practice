b40df5c11636615ed1cd87c01c9ee8d7130fbf81
Fausto Alvarez <faustoalvi@gmail.com>
2022-08-24 09:56:10 -0500

    tutorial lessons

--- learning/models/example/playing_with_tests.sql
+++ learning/models/example/playing_with_tests.sql
@@ -1,4 +1,6 @@
-select c_custkey, c_acctbal
+--select c_custkey, c_acctbal
+select *
 from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."CUSTOMER"
-group by c_custkey, c_acctbal
-having sum(c_acctbal) < 200000000 
+--group by c_custkey, c_acctbal
+group by c_custkey, c_acctbal, c_name, c_address, c_nationkey, c_phone, C_MKTSEGMENT, c_comment
+having sum(c_acctbal) < 200000000
